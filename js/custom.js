80d46873f1122be8240dd18a6b120a89fa3c3fb3
norickkevorkov <norickkevorkov@gmail.com>
2016-06-13 17:35:59 +0300

    completion of the gallery

--- js/custom.js
+++ js/custom.js
@@ -3,4 +3,24 @@
 });
 $(document).ready(function (){
    $('.gallery').enllax();
+   $('.gallery_box').masonry({
+      itemSelector: '.grid-item'
+   });
+   $('.gallery_box').magnificPopup({
+      delegate: 'a',
+      type: 'image',
+      tLoading: 'Loading image #%curr%...',
+      mainClass: 'mfp-img-mobile',
+      gallery: {
+         enabled: true,
+         navigateByImgClick: true,
+         preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+      },
+      image: {
+         tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+         titleSrc: function(item) {
+            return '<small>I Like Trip</small>';
+         }
+      }
+   });
 });
